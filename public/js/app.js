35fdb05495e76c095db6856cc2942202e4c1d61f
Karthikeyan <karthikeyanfz1111@gmail.com>
2023-06-11 13:45:10 +0530

    db partial

--- public/js/app.js
+++ public/js/app.js
@@ -1,69 +1,95 @@
-const express=require("express");
-const bodyparser=require("body-parser");
-const mongoose=require("mongoose");
-const date=require(__dirname+"/date.js");
+const express = require("express");
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+const date = require(__dirname + "/date.js");
 
-const app=express();
-app.use(bodyparser.urlencoded({extended:true}));
-app.set('view engine','ejs');
+const app = express();
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
-mongoose.connect("mongodb://127.0.0.1:27017/todolist",{useNewUrlParser:true});
+mongoose.connect("mongodb://127.0.0.1:27017/todolist", {
+  useNewUrlParser: true
+});
 
-const todolistschema=new mongoose.Schema({
-  name:String
+const todolistschema = new mongoose.Schema({
+  name: String
 })
 
-const List= mongoose.model("List",todolistSchema);
+const List = mongoose.model("List", todolistschema);
 
-
-let items=[];
-let workItems=[];
-
-app.get("/",function(req,res){
-  let day=date.gdate();
-  let greet=date.greet();
-    res.render("dolist",{
-      whatDay:day,
-      newList:items,
-      greeting:greet
-    });
+const l1 = new List({
+  name: "Have to do homework",
 });
-
-app.post("/",function(req,res)
-{
-const item={
-list:req.body.newItem,
-start:req.body.sTime,
-end:req.body.eTime
-};
-if(req.body.submit ==="Work-List")
-{
-  workItems.push(item);
-  res.redirect("/work");
-}
-else{
-  items.push(item);
-  res.redirect("/");
-}
+const l2 = new List({
+  name: "Have to do excerise",
 });
+const l3 = new List({
+  name: "Have to do eat veggie",
+});
+const list = [l1, l2, l3];
 
+let workItems = [];
 
-app.get("/work",function(req,res){
-  let greet=date.greet();
-  res.render("dolist",{
-    whatDay:"Work-List",
-    newList:workItems,
-    greeting:greet
+app.get("/", function(req, res) {
+  let day = date.gdate();
+  let greet = date.greet();
+
+  List.find({}).then(data => {
+    if(data.length==0)
+    {
+      List.insertMany(list).then(data => {
+        console.log("sucess");
+      });
+        res.redirect("/");
+    }
+    else{
+        res.render("dolist", {whatDay: day,newList: data,greeting: greet});
+    }
   });
 });
 
-app.post("/work",function(req,res)
-{
-let item=req.body.newItem;
-workItems.push(item);
-res.redirect("/work");
+app.post("/", function(req, res) {
+  const item = new List({
+    name: req.body.newItem
+  });
+  item.save();
+  res.redirect("/");
+  // start:req.body.sTime,
+  // end:req.body.eTime
+  // if(req.body.submit ==="Work-List")
+  // {
+  //   workItems.push(item);
+  //   res.redirect("/work");
+  // }
+  // else{
+  //   items.push(item);
+  //   res.redirect("/");
+  // }
+});
+app.post("/delete", function(req, res) {
+  const id=req.body.checkbox;
+  List.findByIdAndRemove(id).then(data => {
+    console.log("delete sucess");
+    res.redirect("/");
+  });
 });
 
-app.listen(3000,function(req,res){
-    console.log("server is starting...");
+
+
+app.get("/work", function(req, res) {
+  let greet = date.greet();
+  res.render("dolist", {whatDay: "Work-List",newList: workItems,greeting: greet
+  });
+});
+
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.listen(3000, function(req, res) {
+  console.log("server is starting...");
 });
--- public/js/date.js
+++ public/js/date.js
@@ -1,37 +1,30 @@
-exports.gdate=function (){
-  let today=new Date();
-  let option={
-    weekday:"long",
-    day:"numeric",
-    month:"long",
-    year:"numeric",
-    hour:"2-digit",
-    minute:"2-digit",
+exports.gdate = function() {
+  let today = new Date();
+  let option = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
     hour12: false
   };
-let day=today.toLocaleString("en-US",option);
-return day;
+  let day = today.toLocaleString("en-US", option);
+  return day;
 }
 
-exports.greet=function ()
-{
-  let today=new Date();
-  let hour=today.getHours("en-US");
-  let greet="";
-  if(hour>=1 && hour<=12)
-  {
-     greet="Good Morning!!";
-  }
-  else if(hour>12 && hour<=14)
-  {
-     greet="Good Afternoon!!";
-  }
-  else if(hour>14 && hour<=17)
-  {
-     greet="Good Evening!!";
-  }
-  else{
-     greet="Good Night!!";
+exports.greet = function() {
+  let today = new Date();
+  let hour = today.getHours("en-US");
+  let greet = "";
+  if (hour >= 1 && hour <= 12) {
+    greet = "Good Morning!!";
+  } else if (hour > 12 && hour <= 14) {
+    greet = "Good Afternoon!!";
+  } else if (hour > 14 && hour <= 17) {
+    greet = "Good Evening!!";
+  } else {
+    greet = "Good Night!!";
   }
   return greet;
 }
